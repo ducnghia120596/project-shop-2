a5d027d241760d9ba2817305d26833d4b13e9285
ducnghia1205 <ducnghia1205@gmail.com>
2018-01-24 16:00:06 +0700

    Bai 7 Trien khai repository va migration database Note: - Triển khai Repository cho từng model - Migration ------ enable-migrations ------ add-migration IntitalDB ------ update-database ( chú ý : thêm thuộc tính providerName="System.Data.SqlClient" cho  App.config( Data) ...và Web.Config(WEb)

--- TeduShop/TeduShop.Model/Models/OrderDetail.cs
+++ TeduShop/TeduShop.Model/Models/OrderDetail.cs
@@ -7,17 +7,21 @@ namespace TeduShop.Model.Models
     public class OrderDetail
     {
         [Key]
+        [Column(Order = 1)]
         public int OrderID { set; get; }
 
         [Key]
+        [Column(Order = 2)]
         public int ProductID { set; get; }
 
         public int Quantitty { set; get; }
 
         [ForeignKey("OrderID")]
+        
         public virtual Order Order { set; get; }
 
         [ForeignKey("ProductID")]
+        
         public virtual Product Product { set; get; }
     }
 }
