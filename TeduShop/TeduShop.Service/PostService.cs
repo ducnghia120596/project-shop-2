2b13ae82e49555e19a5fd5e14114b9d547ea2f31
ducnghia1205 <ducnghia1205@gmail.com>
2018-01-25 19:45:58 +0700

    Bai 8 Trien khai tang Service cho du an Commit 3: Tạo interface rồi implement

---
+++ TeduShop/TeduShop.Service/PostService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+using System.Linq;
+namespace TeduShop.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+        Post GetById(int id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+        IEnumerable<Post> GetByTagPaging(string tag, int page, int pageSize, out int totalRow);
+        void SaveChanges();
+    }
+    public class PostService : IPostService
+    {
+        IPostRepository _postRepository;
+        IUnitOfWork _unitOfWork;
+        public PostService(IPostRepository postRepository,IUnitOfWork unitOfWork)
+        {
+            this._postRepository = postRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void Add(Post post)
+        {
+            _postRepository.Add(post);
+        }
+
+        public void Delete(int id)
+        {
+            _postRepository.Delete(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _postRepository.GetAll(new string[] { "PostCategory"});
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public Post GetById(int id)
+        {
+            return _postRepository.GetSingleById(id);
+        }
+
+        public IEnumerable<Post> GetByTagPaging(string tag, int page, int pageSize, out int totalRow)
+        {
+            // Todo: Select all post by Tag ( edit PostRepository)
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Post post)
+        {
+            _postRepository.Update(post);
+        }
+    }
+}
