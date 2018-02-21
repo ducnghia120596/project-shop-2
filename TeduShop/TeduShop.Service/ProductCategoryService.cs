ef783a8ec5af74076c5e16d8cd3f261bb2871606
ducnghia1205 <ducnghia1205@gmail.com>
2018-02-21 12:02:12 +0700

    Bai 2 23 Doc danh sach du lieu tu WebAPI su dung AngularJS Note: Commit lan 1 Tao Service cho PostCategory

---
+++ TeduShop/TeduShop.Service/ProductCategoryService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IProductCategoryService
+    {
+        ProductCategory Add(ProductCategory ProductCategory);
+        void Update(ProductCategory ProductCategory);
+        ProductCategory Delete(int id);
+        IEnumerable<ProductCategory> GetAll();
+        IEnumerable<ProductCategory> GetAllByParentID(int parentID);
+        ProductCategory GetByID(int id);
+        void Save();
+    }
+    public class ProductCategoryService : IProductCategoryService
+    {
+        IProductCategoryRepository _ProductCategoryRepository;
+        IUnitOfWork _unitOfWork;
+
+        public ProductCategoryService(IProductCategoryRepository ProductCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._ProductCategoryRepository = ProductCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public ProductCategory Add(ProductCategory ProductCategory)
+        {
+            return _ProductCategoryRepository.Add(ProductCategory);
+        }
+
+        public ProductCategory Delete(int id)
+        {
+            return _ProductCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<ProductCategory> GetAll()
+        {
+            return _ProductCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAllByParentID(int parentID)
+        {
+            return _ProductCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentID);
+        }
+
+        public ProductCategory GetByID(int id)
+        {
+            return _ProductCategoryRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(ProductCategory ProductCategory)
+        {
+            _ProductCategoryRepository.Update(ProductCategory);
+        }
+    }
+}
