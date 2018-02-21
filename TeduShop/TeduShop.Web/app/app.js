60f964e341fff48cb89a51812af7bed60d9fce07
ducnghia1205 <ducnghia1205@gmail.com>
2018-02-21 10:21:13 +0700

    Bai 22 Ghep giao dien quan tri va trien khai routing cho ung dung Note: Commit lan 3

--- TeduShop/TeduShop.Web/app/app.js
+++ TeduShop/TeduShop.Web/app/app.js
@@ -1,1 +1,15 @@
-﻿
+﻿/// <reference path="D:\Nam4\LEARN\ASP.net Angular\Demo\Git\project-shop-2\TeduShop\TeduShop.Web\bower_components/angular/angular.js" />
+(function(){
+    angular.module('tedushop', ['tedushop.products','tedushop.common']).config(config);
+
+    config.$inject = ['$stateProvider','$urlRouterProvider'];
+
+    function config($stateProvider,$urlRouterProvider) {
+        $stateProvider.state('home',{
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller:"homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
