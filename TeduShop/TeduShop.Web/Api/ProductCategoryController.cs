426d4d7889ba0c9d4c48cd72fb722698be020ef0
ducnghia1205 <ducnghia1205@gmail.com>
2018-02-23 14:24:37 +0700

    Bai 2 25 Phan trang danh sach trong AngularJS Note: Backend 1. Tạo class generic chứa thông tin trả về 2. Áp dụng vào phân trang cho WebApi ProductCategoryController ( sử dụng LinQ )

--- TeduShop/TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop/TeduShop.Web/Api/ProductCategoryController.cs
@@ -25,13 +25,27 @@ namespace TeduShop.Web.Api
             this._productCategoryService = productCategoryService;
         }
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page,int pageSize=20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                totalRow = model.Count();
+
+                var query = model.OrderBy(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategoryViewModel>>(query);
+
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
