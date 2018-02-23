c2db9721ec649da3b8f898b9630b24ab094faba3
ducnghia1205 <ducnghia1205@gmail.com>
2018-02-23 14:00:55 +0700

    Bai 2 24 Cach su dung Filter de dinh dang du lieu Note: Tạo filter thủ công

---
+++ TeduShop/TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="D:\Nam4\LEARN\ASP.net Angular\Demo\Git\project-shop-2\TeduShop\TeduShop.Web\bower_components/angular/angular.js" />
+(function (app) {
+    app.filter('statusFilter',function(){
+        return function(input){
+            if (input == true) {
+                return 'Kích hoạt';
+            }
+            else
+            {
+                return 'Khóa';
+            }
+        }
+    });
+})(angular.module('tedushop.common'));
