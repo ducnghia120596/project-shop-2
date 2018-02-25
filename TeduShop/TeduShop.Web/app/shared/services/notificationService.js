659117e856c0e2dcf4a3f7b819184f3430d8e09e
ducnghia1205 <ducnghia1205@gmail.com>
2018-02-25 10:49:34 +0700

    Bai 2 27 Trien khai notification cho nguoi dung su dung toastr Note: commit lan 2 - chèn link vào file index - triển khai notìificationService ... inject service vào Controller - nhúng link js vào file index

---
+++ TeduShop/TeduShop.Web/app/shared/services/notificationService.js
@@ -0,0 +1,45 @@
+﻿(function (app) {
+    app.factory('notificationService', notificationService);
+
+    function notificationService() {
+        toastr.option = {
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "fadeIn": 300,
+            "fadeOut": 1000,
+            "timeOut": 3000,
+            "extendedTimeOut":1000
+        };
+
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayWarning: displayWarning,
+            displayInfo: displayInfo
+        }
+
+        function displaySuccess(message) {
+            toastr.success(message);
+        }
+
+        function displayError(error) {
+            if (Array.isArray(error)) {
+                error.each(function (err) {
+                    toastr.errror(err);
+                });
+            }
+            else {
+                toastr.error(error);
+            }
+        }
+
+        function displayWarning(message) {
+            toastr.warning(message);
+        }
+
+        function displayInfo(message) {
+            toastr.info(message);
+        }
+    }
+})(angular.module('tedushop.common'));
