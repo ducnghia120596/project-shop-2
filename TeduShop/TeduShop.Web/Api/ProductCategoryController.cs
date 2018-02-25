dfb902c344d6096777030c77aef7e65b3d1a29da
ducnghia1205 <ducnghia1205@gmail.com>
2018-02-25 11:15:53 +0700

    Bai 2 28 Them moi du lieu va validate form Note: Commit lan 1 - Tạo phương thức api thêm mới dữ liệu cho ProductCategory - thêm method updateProductCategory ở EntityExtension

--- TeduShop/TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop/TeduShop.Web/Api/ProductCategoryController.cs
@@ -9,9 +9,7 @@ using TeduShop.Model.Models;
 using TeduShop.Service;
 using TeduShop.Web.infrastructure.Core;
 using TeduShop.Web.Models;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using TeduShop.Web.infrastructure.Extensions;
 
 namespace TeduShop.Web.Api
 {
@@ -24,7 +22,9 @@ namespace TeduShop.Web.Api
         {
             this._productCategoryService = productCategoryService;
         }
+
         [Route("getall")]
+        [HttpGet]
         public HttpResponseMessage GetAll(HttpRequestMessage request,string keyword, int page,int pageSize=20)
         {
             return CreateHttpResponse(request, () =>
@@ -46,6 +46,27 @@ namespace TeduShop.Web.Api
                     TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
                 };
                 var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
+                return response;
+            });
+        }
+        [Route("create")]
+        [HttpPost]
+        public HttpResponseMessage Create(HttpRequestMessage request,ProductCategoryViewModel productCategoryVM)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                var newProductCategory = new ProductCategory();
+
+                newProductCategory.UpdateProductCategory(productCategoryVM);
+                _productCategoryService.Add(newProductCategory);
+                _productCategoryService.Save();
+                var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(newProductCategory);
+                response = request.CreateResponse(HttpStatusCode.Created, responseData);
                 return response;
             });
         }
