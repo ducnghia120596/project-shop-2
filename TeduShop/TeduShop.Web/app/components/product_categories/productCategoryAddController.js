d65e73dbc664771e9650480a6ea40a56fb7a2b9d
NghiaDang <ducnghia1205@gmail.com>
2018-06-20 22:21:09 +0700

    thêm GetSeoTitle cho Add ProductCategory

--- TeduShop/TeduShop.Web/app/components/product_categories/productCategoryAddController.js
+++ TeduShop/TeduShop.Web/app/components/product_categories/productCategoryAddController.js
@@ -1,12 +1,17 @@
 ﻿(function (app) {
     app.controller("productCategoryAddController", productCategoryAddController);
 
-    productCategoryAddController.$inject = ['$scope','apiService','notificationService','$state'];
+    productCategoryAddController.$inject = ['$scope', 'apiService', 'notificationService', '$state', 'commonService'];
     // $state thuộc ui-router angular ( cơ chế điều hướng )
-    function productCategoryAddController($scope, apiService, notificationService, $state) {
+    function productCategoryAddController($scope, apiService, notificationService, $state, commonService) {
         $scope.productCategory = {
             CreatedDate: new Date(),
             Status: true,
+        }
+        // tự động sinh Alias
+        $scope.GetSeoTitle = GetSeoTitle;
+        function GetSeoTitle() {
+            $scope.productCategory.Alias = commonService.getSeoTitle($scope.productCategory.Name);
         }
         // phương thức để submit
         $scope.AddProductCategory = AddProductCategory;
